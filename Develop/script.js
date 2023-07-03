55d26e0591b9db9d3ab0ca61df0c5a36cd3c1203
Vivid Quill <sjdoublec@gmail.com>
2023-07-03 10:46:59 -0500

    Updated JavaScript that seems to hit all the requirements

--- Develop/script.js
+++ Develop/script.js
@@ -1,40 +1,74 @@
-// Assignment code here
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const symbols = ["@", "#", "$", "%", "&", "*"];
-const characterCodes = Array.from(Array(26)).map((_, i) => i + 97);
-const lowercaseLetters = characterCodes.map(code => String.fromCharCode(code));
-const uppercaseLetters = lowercaseLetters.map(letter => letter.toUpperCase());
+// Connects code to HTML "Generate Password" red button.
+document.getElementById("generate").addEventListener("click", generatePassword); 
 
-console.log(uppercaseLetters);
+// Code block that asks user to choose the length of their password and what it will contain.
+function generatePassword() {
+  let passwordLength = prompt(
+    "Your password must be between 8 and 128 characters.\nHow long do you want it to be?"
+    );
+    if (passwordLength >= 8 && passwordLength <= 128) {
 
+      let hasNumbers = confirm(
+        "Click 'Okay' if you want numbers in your password.\nClick 'Cancel' if you do not."
+        );
+      let hasSymbols = confirm(
+        "Click 'Okay' if you want symbols in your password.\nClick 'Cancel' if you do not."
+        );
+      let hasLowercaseLetters = confirm(
+        "Click 'Okay' if you want lowercase letters in your password.\nClick 'Cancel' if you do not."
+        );
+      let hasUppercaseLetters = confirm(
+        "Click 'Okay' if you want uppercase letters in your password.\nClick 'Cancel' if you do not."
+        );
+      
+      // Empty array to put numbers, symbols, lowercaseLetters, and uppercaseLetters into.
+      let availableCharacters = [];
+      
+      // Arrays of numbers, letters, lowercaseLetters, and uppercaseLetters that will be put into availableCharacters.
+      const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+      const symbols = ["@", "#", "$", "%", "&", "*"];
+      const lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+      const uppercaseLetters = lowercaseLetters.map(lowercaseLetter => lowercaseLetter.toUpperCase());
+  
+      // Series of if statements that help determine if numbers, letters, lowercaseLetters, and uppercaseLetters will be added to availableCharacters.
+      if (hasNumbers) {
+        availableCharacters = availableCharacters.concat(numbers)
+      };
 
-// alert("Your password must be between 8 and 128 characters. How many characters do you want your password to be?");
+      if (hasSymbols) {
+        availableCharacters = availableCharacters.concat(symbols)
+      };
 
-// alert("Your password must have at least one of the following:");
+      if (hasLowercaseLetters) {
+        availableCharacters = availableCharacters.concat(lowercaseLetters)
+      };
 
-// alert("Uppercase");
+      if (hasUppercaseLetters) {
+        availableCharacters = availableCharacters.concat(uppercaseLetters)
+      };
+  
+      // Empty password string.
+      let password = "";
 
-// alert("Lowercase");
+      // If statement to check that user has selected at least one of numbers, letters, lowercaseLetters, or uppercaseLetters.
+      if (availableCharacters.length === 0) {
+        alert("You need to select at least one of numbers, symbols, lowercase letters, or uppercase letters.\nPlease refresh your browser to restart the process."
+        );
+        // Empty string that shows up in "Generate Password" HTML box.
+        return "";
+      }
 
-// alert("Numbers");
-
-// alert("Symbols");
-
-// alert("You did not choose at least one of uppercase, lowercase, numbers, or symbols!")
-
-// alert("Your password is: ")
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+      // Code block that creates the random password for the user.
+      for (let i = 0; i < passwordLength; i++) {
+        const randomIndex = Math.floor(Math.random() * availableCharacters.length);
+        password += availableCharacters[randomIndex];
+      };
+      // Code that sends the random password to the "Generate Password" HTML box.
+      var passwordText = document.querySelector("#password");
+      passwordText.value = password;
+    }
+    // Alert in case user chose a password less than 8 characters or greater than 128 characters.
+    else (alert(
+    "Your requested password length is less than 8 characters or more than 128 characters.\nPlease refresh your browser to restart the process."
+  ));
+}  
